c5f833fdeb13e5011fe11c31649ddc88701689f8
giovannibonin <giovannibonin@rlm07.itlabs.miami.edu>
2016-12-09 04:43:21 -0500

    basics

---
+++ configTools.sh
@@ -0,0 +1,9 @@
+# This is where the magic happens (runs everything and wraps it up)
+
+
+
+
+# Vim Config
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+

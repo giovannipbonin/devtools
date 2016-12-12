3a7a674db0723f3e52191b965fd9fc9ac1b34ac9
giovannibonin <giovannibonin@rlm07.itlabs.miami.edu>
2016-12-11 19:54:44 -0500

    update

--- configTools.sh
+++ configTools.sh
@@ -26,7 +26,8 @@ git clone https://github.com/vim-scripts/quickrun.vim
 cd ~/devtools
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 open solarized.itermcolors
-ln .zshrc ~/.zshrc
-ln .bash_profile ~/.bash_profile
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git  
+ln .zshrc ~/.zshrc
+ln .bash_profile ~/.bash_profile
+source ~/.bash_profile
